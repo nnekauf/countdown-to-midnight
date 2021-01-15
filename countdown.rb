18fdfe36c87047c66bd2d7acf73295144a914e31
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-01-15 17:15:09 -0500

    commit tree 25ca3b0134df9113ea3f5d80b15d9d46989f0d27

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
+    
 
-def countdown
-end
+    integer = 10
+
+    def countdown(integer)
+    while integer > 0
+        puts "#{integer} SECOND(S)!"
+        integer -= 1
+    end
+        return "HAPPY NEW YEAR!"
+    end
+
+    def countdown_with_sleep(timer)
+        sleep 5
+    end
